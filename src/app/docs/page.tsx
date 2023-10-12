f7f8534ada02df6605e875a7ffaa510a3c686e11
배성완 <baesee@kakao.com>
2023-10-12 13:25:58 +0900

    design: 'docs,동적 라우팅 구현'
    Relate to:#2

--- src/app/docs/page.tsx
+++ src/app/docs/page.tsx
@@ -1,5 +1,38 @@
-const docs = () => {
-  return <div>page</div>;
+"use client";
+import { useRouter } from "next/navigation";
+import styles from "../../styles/Docs.module.css";
+interface Items {
+  id: number;
+  name: string;
+  bgc: string;
+  color: string;
+}
+const Docs = () => {
+  const router = useRouter();
+  const items: Items[] = [
+    { id: 1, name: "Javascript", bgc: "#F7E029", color: "#000000" },
+    { id: 2, name: "React", bgc: "#20D9FE", color: "#ffffff" },
+    { id: 3, name: "Next.JS", bgc: "#3c3c3c", color: "#ffffff" },
+  ];
+  const handleRoute = (value: string) => {
+    router.push(`/docs/${value}`);
+  };
+  return (
+    <div className={styles.container}>
+      {items.map((item) => {
+        return (
+          <div
+            key={item.id}
+            className={styles.docsItem}
+            style={{ backgroundColor: `${item.bgc}`, color: `${item.color}` }}
+            onClick={() => handleRoute(item.name)}
+          >
+            {item.name}
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
-export default docs;
+export default Docs;
